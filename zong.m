7acec3028027446fbc15346006000b3a4fd3a0f2
IgnaceVS <ignace.vanspilbeeck@uantwerpen.be>
2022-12-20 14:28:38 +0100

    Authors

--- zong.m
+++ zong.m
@@ -1,6 +1,7 @@
 function zong()
 % Function to create GUI
-% Author: Ignace Van Spilbeeck, Bio-Imaging Lab, University of Antwerp
+% Authors: Ignace Van Spilbeeck, Christien Bowman
+%   Bio-Imaging Lab, University of Antwerp
 % 2021/02/22
     gui = GUIHandle();
 end
